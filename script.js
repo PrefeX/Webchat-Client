16789171f3b64fda6ebc137d4fa9a6278aa07ec9
PrefeX <christianrelling@hotmail.com>
2018-08-27 17:25:07 +0200

    Corrected wrong folder structure, resulting in wrong files

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-var username = getParam("name");
+// Read the entered value from the message field
+function readInputField() {
+    return document.getElementById("chatMessage").value;
+}
 
-// TODO: Implement
-function refresh() {
-    $(chatWindow).append("<p>Refreshing...</p>");
+// Clear the input field for the message
+function clearInputField() {
+    document.getElementById("chatMessage").value = "";
+}
+
+// Scroll to bottom
+function scrollToBottom() {
+    // TODO: Fix for new layout
+    var objDiv = document.getElementById("chatWindow");
+    objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+function sendMessage(msgType = 'text') {
+    var message = readInputField();
+
+    // Make sure there is a input
+    if (message != "") {
+
+        if (checkURL(message) == true || msgType == 'image') {
+            message = "<a href=\"" + message + "\" target=\"_blank\"><img src=\"" + message + "\"></a>";
+        }
+
+        $(chatWindow).append("<div class=\"chat-outgoing align-self-start bg-primary text-white p-2 mb-3\">" + message + "</div>")
+        clearInputField();
+    }
+}
+
+
+function recieveMessage() {
+    $(chatWindow).append("<div class=\"chat-incomming align-self-end bg-info text-white p-2 mb-3\">" + readInputField() + "</div>")
+}
+
+
+function checkURL(url) {
+    return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
 }
 
 
 // Send a message to the server, and clear the users input field
 // Example IMG: https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png
-function submitMessage(type) {
+/* function submitMessage(type) {
 
     var codePrefix = "<p><b>" + username + ":</b> ";
     var codeSuffix = "</p>";
@@ -28,42 +63,4 @@ function submitMessage(type) {
         clearInputField();
         scrollToBottom();
     }
-}
-
-
-// Return the current message
-function readInputField() {
-    return document.getElementById("chatMessage").value;
-}
-
-// Clear the input field
-function clearInputField() {
-    document.getElementById("chatMessage").value = "";
-}
-
-// Scroll to bottom
-function scrollToBottom() {
-    var objDiv = document.getElementById("chatWindow");
-    objDiv.scrollTop = objDiv.scrollHeight;
-}
-
-
-// Extracts the value of a given parameter from the url
-function getParam(sname) {
-    var params = location.search.substr(location.search.indexOf("?") + 1);
-    var sval = "";
-    params = params.split("&");
-    // split param and value into individual pieces
-    for (var i = 0; i < params.length; i++) {
-        var temp = params[i].split("=");
-        if (temp[0] === sname) {
-            sval = temp[1];
-        }
-    }
-
-    // If requesting the username, mark blank names as Guest
-    if (sname == "name" && sval == "") {
-        sval = "Guest"
-    }
-    return sval;
-}
+} */
